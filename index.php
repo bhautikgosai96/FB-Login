3534c11cf304bcef1b860f5ad69ebc6f78d50817
Bhautik Gosai <bhautikng143@gmail.com>
2017-06-06 15:37:43 +0530

    start2

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
+
 <?php
 session_start();
 require_once __DIR__ . '/src/Facebook/autoload.php';
-
 $fb = new Facebook\Facebook([
   'app_id' => '120783235172145',
   'app_secret' => '2af58b7080bcb06278ad922a787f27a2',
-  'default_graph_version' => 'v2.5'
+  'default_graph_version' => 'v2.4',
   ]);
-
 $helper = $fb->getRedirectLoginHelper();
-
-// app directory could be anything but website URL must match the URL given in the developers.facebook.com/apps
-define('APP_URL', 'https://bhautikng143.herokuapp.com/');
-
-$permissions = ['manage_pages']; // optional
+$permissions = ['email']; // optional
 
 try {
 	if (isset($_SESSION['facebook_access_token'])) {
@@ -24,47 +19,40 @@ try {
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
  	// When Graph returns an error
  	echo 'Graph returned an error: ' . $e->getMessage();
-
   	exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
  	// When validation fails or other local issues
 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
   	exit;
  }
-
 if (isset($accessToken)) {
 	if (isset($_SESSION['facebook_access_token'])) {
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	} else {
 		// getting short-lived access token
 		$_SESSION['facebook_access_token'] = (string) $accessToken;
-
 	  	// OAuth 2.0 client handler
 		$oAuth2Client = $fb->getOAuth2Client();
-
 		// Exchanges a short-lived access token for a long-lived one
 		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-
 		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-
 		// setting default access token to be used in script
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	}
-
 	// redirect the user back to the same page if it has "code" GET variable
 	if (isset($_GET['code'])) {
 		header('Location: ./');
 	}
-
-	// validating user access token
+	// getting basic info about user
 	try {
-		$user = $fb->get('/me');
-		$user = $user->getGraphNode()->asArray();
+		$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+		$profile = $profile_request->getGraphNode()->asArray();
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
 		// When Graph returns an error
 		echo 'Graph returned an error: ' . $e->getMessage();
 		session_destroy();
-		// if access token is invalid or expired you can simply redirect to login page using header() function
+		// redirecting user back to app login page
+		header("Location: ./");
 		exit;
 	} catch(Facebook\Exceptions\FacebookSDKException $e) {
 		// When validation fails or other local issues
@@ -72,34 +60,11 @@ if (isset($accessToken)) {
 		exit;
 	}
 
-	$pages = $fb->get('/me/accounts');
-	$pages = $pages->getGraphEdge()->asArray();
-
-	?>
-	<form action="" method="POST">
-		<select name="page" single>
-	<?php
-	foreach ($pages as $key) {
-		?>
-		<option value="<?php echo $key['id']; ?>"><?php echo $key['name']; ?></option>
-		<?php
-	}
-	?>
-	</select>
-	<input type="submit" name="submit">
-	</form>
-	<?php
-	if (isset($_POST['submit'])) {
-		$page = $fb->get('/' . $_POST['page'] . '?fields=access_token, name, id');
-		$page = $page->getGraphNode()->asArray();
-
-		$addTab = $fb->post('/' . $page['id'] . '/tabs', array('app_id' => 'APP_ID'), $page['access_token']);
-		$addTab = $addTab->getGraphNode()->asArray();
-		print_r($addTab);
-	}
+	// printing $profile array on the screen which holds the basic info about user
+	print_r($profile);
   	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
 	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-	$loginUrl = $helper->getLoginUrl('https://bhautikng143.herokuapp.com/index.php', $permissions);
+	$loginUrl = $helper->getLoginUrl('https://bhautikng143.herokuapp.com/', $permissions);
 	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
