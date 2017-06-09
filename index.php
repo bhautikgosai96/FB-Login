e763e272d02c24aafa23a18adfc8a606aa39a186
Bhautik Gosai <bhautikng143@gmail.com>
2017-06-09 12:02:43 +0530

    start4

--- index.php
+++ index.php
@@ -79,30 +79,17 @@ if (isset($accessToken)) {
     	}
 
     	// showing picture on the screen
-    	echo "<img src='".$picture['url']."'/>";
-    	//echo "";
+    	//echo "<img src='".$picture['url']."'/>";
+    	echo "image";
 
-    	/*<html>
-    	    <head>
-    	        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    	        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    	    </head>
+    	<html>
     	    <body>
-                   <div class="container">
-                        <div class="row">
-                            <div class="col-md-offset-1 col-md-5">
-                                <img src="<?php echo '$picture['url']'; ?>"/>
-                            </div>
-                            <div class="col-md-offset-1 col-md-5">
-                                <?php echo $profile->getName(); ?>
-                            </div>
-                        </div>
-                   </div>
+                <img src="<?php echo $picture['url']; ?>"/>
+                <p><?php echo $profile->getName(); ?></p>
     	    </body>
-    	</html>*/
+    	</html>
 
+        echo " ";
     	echo "success";
   	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
