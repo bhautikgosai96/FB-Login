906cae57d1bc0e4821b4f8c106bb0be274bdad06
Bhautik Gosai <bhautikng143@gmail.com>
2017-06-13 14:07:04 +0530

    start4

--- index.php
+++ index.php
@@ -1,4 +1,107 @@
 <?php
+if(!session_id()){
+    session_start();
+}
+
+/*
+ * Get access token using Facebook Graph API
+ */
+if(isset($_SESSION['facebook_access_token'])){
+    // Get access token from session
+    $access_token = $_SESSION['facebook_access_token'];
+}else{
+    // Facebook app id & app secret
+    $appId = '120783235172145';
+    $appSecret = '2af58b7080bcb06278ad922a787f27a2';
+
+    // Generate access token
+    $graphActLink = "https://graph.facebook.com/oauth/access_token?client_id={$appId}&client_secret={$appSecret}&grant_type=client_credentials";
+
+    // Retrieve access token
+    $accessTokenJson = file_get_contents($graphActLink);
+    $accessTokenObj = json_decode($accessTokenJson);
+    $access_token = $accessTokenObj->access_token;
+
+    // Store access token in session
+    $_SESSION['facebook_access_token'] = $access_token;
+}
+
+// Get photo albums of Facebook page using Facebook Graph API
+$fields = "id,name,description,link,cover_photo,count";
+$fb_page_id = "120783235172145";
+$graphAlbLink = "https://graph.facebook.com/v2.9/{$fb_page_id}/albums?fields={$fields}&access_token={$access_token}";
+
+$jsonData = file_get_contents($graphAlbLink);
+$fbAlbumObj = json_decode($jsonData, true, 512, JSON_BIGINT_AS_STRING);
+
+// Facebook albums content
+$fbAlbumData = $fbAlbumObj['data'];
+?>
+Loop through the retrieved album’s content of the Facebook page and list all the albums with the cover photo, name, and description.
+
+<?php
+// Render all photo albums
+foreach($fbAlbumData as $data){
+    $id = isset($data['id'])?$data['id']:'';
+    $name = isset($data['name'])?$data['name']:'';
+    $description = isset($data['description'])?$data['description']:'';
+    $link = isset($data['link'])?$data['link']:'';
+    $cover_photo_id = isset($data['cover_photo']['id'])?$data['cover_photo']['id']:'';
+    $count = isset($data['count'])?$data['count']:'';
+
+    $pictureLink = "photos.php?album_id={$id}&album_name={$name}";
+
+    echo "<div class='fb-album'>";
+    echo "<a href='{$pictureLink}'>";
+    echo "<img src='https://graph.facebook.com/v2.9/{$cover_photo_id}/picture?access_token={$access_token}' alt=''>";
+    echo "</a>";
+    echo "<h3>{$name}</h3>";
+
+    $photoCount = ($count > 1)?$count. 'Photos':$count. 'Photo';
+
+    echo "<p><span style='color:#888;'>{$photoCount} / <a href='{$link}' target='_blank'>View on Facebook</a></span></p>";
+    echo "<p>{$description}</p>";
+    echo "</div>";
+}
+?>
+
+<?php
+if(!session_id()){
+    session_start();
+}
+
+// Get album id from url
+$album_id = isset($_GET['album_id'])?$_GET['album_id']:header("Location: index.php");
+$album_name = isset($_GET['album_name'])?$_GET['album_name']:header("Location: index.php");
+
+// Get access token from session
+$access_token = $_SESSION['facebook_access_token'];
+
+// Get photos of Facebook page album using Facebook Graph API
+$graphPhoLink = "https://graph.facebook.com/v2.9/{$album_id}/photos?fields=source,images,name&access_token={$access_token}";
+$jsonData = file_get_contents($graphPhoLink);
+$fbPhotoObj = json_decode($jsonData, true, 512, JSON_BIGINT_AS_STRING);
+
+// Facebook photos content
+$fbPhotoData = $fbPhotoObj['data'];
+?>
+
+
+<?php
+// Render all photos
+foreach($fbPhotoData as $data){
+    $imageData = end($data['images']);
+    $imgSource = isset($imageData['source'])?$imageData['source']:'';
+    $name = isset($data['name'])?$data['name']:'';
+
+    echo "<div class='fb-album'>";
+    echo "<img src='{$imgSource}' alt=''>";
+    echo "<h3>{$name}</h3>";
+    echo "</div>";
+}
+?>
+
+/*<?php
 session_start();
 require_once __DIR__ . '/src/Facebook/autoload.php';
 
@@ -267,7 +370,7 @@ if (isset($accessToken)) {
 
 </body>
 </html>
-
+*/
 /*<?php
 ini_set('display_errors', 1);
 session_start();
