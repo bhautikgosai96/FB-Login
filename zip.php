020ee1b22d9e35ebee2c5261847107ffe9233b80
Bhautik Gosai <bhautikng143@gmail.com>
2017-06-20 22:39:14 +0530

    start5

--- zip.php
+++ zip.php
@@ -1,5 +1,57 @@
 <?php
 
+$errors = array();
+$data = array();
+
+$postdata = file_get_contents('php://input');
+$request = json_decode($postdata);
+$img = $request;
+
+
+
+$zip = new ZipArchive();
+
+$filename = 'uploads/'.time().'.zip';
+if($zip->open($filename, ZipArchive::CREATE)!=TRUE)
+    die ("Could not open archive");
+
+
+$temp=[];
+foreach($img as $file){
+
+
+	$download_file = file_get_contents($file);
+
+	$name =basename($file);
+	array_push($temp,$name);
+	file_put_contents($name,$download_file);
+
+	$zip->addFile($name);
+
+
+
+}
+
+
+$zip->close();
+//echo 'Exist or not'.file_exists($filename);
+// close zip
+
+foreach($temp as $n){
+
+	unlink($n);
+	//removing file from server
+}
+$filename=array($filename);
+echo json_encode($filename);
+
+
+
+?>
+
+
+/*<?php
+
 $image = json_decode(file_get_contents("php://input"));
 
 $zip = new ZipArchive();
@@ -23,4 +75,4 @@ if(file_exists($filename)){
 echo json_encode($filename);
 
 
-?>
+?>*/
