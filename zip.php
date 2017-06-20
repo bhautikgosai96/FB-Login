acc3a1cf0788e2a73bdbbc582853a224376fb54d
Bhautik Gosai <bhautikng143@gmail.com>
2017-06-20 14:09:25 +0530

    start5

--- zip.php
+++ zip.php
@@ -1,32 +1,50 @@
 <?php
-function createZip($files, $zip_file) {
-   $zip = new ZipArchive;
 
-  if($zip->open($zip_file, ZipArchive::CREATE) === TRUE)
-  {
-    foreach($files as $file)
-    {
-      $zip->addFile($file);
-    }
-    $zip->close();
-    return true;
-  }
-  else return false;
-}
+$errors = array();
+$data = array();
+
 $postdata = file_get_contents('php://input');
 $request = json_decode($postdata);
-$files = $request;
+$img = $request;
 
-$zip_file = 'final.zip';
-if(createZip($files, $zip_file))
-    echo json_encode('The '. $zip_file. ' successfully created');
-else
-    echo json_encode('Unable to create the '. $zip_file. ' file');
 
-header('Content-type: application/zip');
-header('Content-disposition: filename="'. $zip_file. '"');
-header('Content-length:'. filesize($zip_file));
-readfile($zip_file);
-exit();
+
+$zip = new ZipArchive();
+
+$filename = 'uploads/'.time().'.zip';
+if($zip->open($filename, ZipArchive::CREATE)!=TRUE)
+    die ("Could not open archive");
+
+
+$temp=[];
+foreach($img as $file){
+
+
+	$download_file = file_get_contents($file);
+
+	$name =basename($file);
+	array_push($temp,$name);
+	file_put_contents($name,$download_file);
+
+	$zip->addFile($name);
+
+
+
+}
+
+
+$zip->close();
+//echo 'Exist or not'.file_exists($filename);
+// close zip
+
+foreach($temp as $n){
+
+	unlink($n);
+	//removing file from server
+}
+$filename=array($filename);
+echo json_encode($filename);
+
+
 
 ?>
