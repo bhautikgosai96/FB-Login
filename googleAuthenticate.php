b991e6a2479f1e3aed95ce8bbcba62ee1d5a5256
Bhautik Gosai <bhautikng143@gmail.com>
2017-06-30 10:21:32 +0530

    start5

---
+++ googleAuthenticate.php
@@ -0,0 +1,7 @@
+<?php
+require_once '/Zend/Loader.php';
+Zend_Loader::loadClass('Zend_Gdata_Photos');
+Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+Zend_Loader::loadClass('Zend_Gdata_AuthSub');
+
+?>
