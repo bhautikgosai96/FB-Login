8a430a530dca156774858368399c0f1b1138f650
Bhautik Gosai <bhautikng143@gmail.com>
2017-07-02 15:23:46 +0530

    start5

---
+++ googlePhotoUpload.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'Zend/Loader.php';
+
+$postdata = file_get_contents('php://input');
+$request = json_decode($postdata);
+
+$img=$request->img;
+$an=$request->albumnName;
+
+      Zend_Loader::loadClass('Zend_Gdata');
+      Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+      Zend_Loader::loadClass('Zend_Gdata_Photos');
+      Zend_Loader::loadClass('Zend_Http_Client');
+
+      $svc = Zend_Gdata_Photos::AUTH_SERVICE_NAME;
+            $user = "bhautikng143@gmail.com";
+            $pass = "Kevin7872#";
+            $client = Zend_Gdata_ClientLogin::getHttpClient($user, $pass, $svc);
+            $gphoto = new Zend_Gdata_Photos($client);
+
+
+            $albumName = $an;
+
+        try{
+            $photo = $gphoto->newPhotoEntry();
+
+             $file = $gphoto->newMediaFileSource($img[0]);
+             $file->setContentType("image/jpeg");
+             $photo->setMediaSource($file);
+
+              $album = $gphoto->newAlbumQuery();
+              $album->setUser($user);
+              $album->setAlbumName($albumName);
+
+              $gphoto->insertPhotoEntry($photo, $album->getQueryUrl());
+            }catch (Zend_Gdata_App_Exception $e) {
+                     echo "Error: " . $e->getResponse();
+                   }
+
+           echo json_decode($gphoto);
+?>
