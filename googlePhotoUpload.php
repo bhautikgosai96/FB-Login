f942fe4a7f379c293a8af0229e3b5003a981e635
Bhautik Gosai <bhautikng143@gmail.com>
2017-07-03 11:44:18 +0530

    start6

--- googlePhotoUpload.php
+++ googlePhotoUpload.php
@@ -1,5 +1,5 @@
 <?php
-      require_once 'Zend/Loader.php';
+      require_once 'lib/Zend/Loader.php';
       Zend_Loader::loadClass('Zend_Gdata');
       Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
       Zend_Loader::loadClass('Zend_Gdata_Photos');
@@ -15,43 +15,12 @@
       // update the second argument to be CompanyName-ProductName-Version
       $gp = new Zend_Gdata_Photos($client, "Google-DevelopersGuide-1.0");
 
-//$username = "default";
-$filename = "/cricket2.jpg";
-$photoName = "My Test Photo";
-$photoCaption = "The first photo I uploaded to Picasa Web Albums via PHP.";
-$photoTags = "beach, sunshine";
+        $entry = new Zend_Gdata_Photos_AlbumEntry();
+        $entry->setTitle($gp->newTitle("New album"));
+        $entry->setSummary($gp->newSummary("This is an album."));
 
-// We use the albumId of 'default' to indicate that we'd like to upload
-// this photo into the 'drop box'.  This drop box album is automatically
-// created if it does not already exist.
-$albumId = "default";
-
-$fd = $gp->newMediaFileSource($filename);
-$fd->setContentType("image/jpg");
-
-// Create a PhotoEntry
-$photoEntry = $gp->newPhotoEntry();
-
-$photoEntry->setMediaSource($fd);
-$photoEntry->setTitle($gp->newTitle($photoName));
-$photoEntry->setSummary($gp->newSummary($photoCaption));
-
-// add some tags
-$keywords = new Zend_Gdata_Media_Extension_MediaKeywords();
-$keywords->setText($photoTags);
-$photoEntry->mediaGroup = new Zend_Gdata_Media_Extension_MediaGroup();
-$photoEntry->mediaGroup->keywords = $keywords;
-
-// We use the AlbumQuery class to generate the URL for the album
-$albumQuery = $gp->newAlbumQuery();
-
-$albumQuery->setUser($user);
-$albumQuery->setAlbumId($albumId);
-
-// We insert the photo, and the server returns the entry representing
-// that photo after it is uploaded
-$insertedEntry = $gp->insertPhotoEntry($photoEntry, $albumQuery->getQueryUrl());
-      echo 'Photo successfully added!';
+        $createdEntry = $gp->insertAlbumEntry($entry);
+      echo 'Albumn successfully added!';
 ?>
 /*<?php
     $userid = 'bhautikng143@gmail.com';
