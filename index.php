21d3c1ff8c4e66fa86650480ee36cc24774d63ec
Bhautik Gosai <bhautikng143@gmail.com>
2017-07-11 11:15:58 +0530

    start6

--- index.php
+++ index.php
@@ -13,8 +13,28 @@ $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email','user_photos']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('https://bhautikng143.herokuapp.com/fb-callback.php', $permissions);
 
-echo '<a href="' . $loginUrl. '">Log in with Facebook!</a>';
+//echo '<a href="' . $loginUrl. '">Log in with Facebook!</a>';
 session_write_close();
 ?>
 
-
+<html>
+    <head>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <p>PHP Web Engineer Assignment</p>
+                <p>Facebook-Albumn Challenge</p>
+            </div>
+            <div class="row">
+                 <?php echo' <a  href="'.$loginUrl . '">';
+                   echo ' Log in with Facebook';
+                   echo ' </a> '; ?>
+            </div>
+        </div>
+    </body>
+</html>
