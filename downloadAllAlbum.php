ebd6458540a5de0d4e5e8a478a1f90cf838928f5
Bhautik Gosai <bhautikng143@gmail.com>
2017-07-16 15:58:04 +0530

    start6

--- downloadAllAlbum.php
+++ downloadAllAlbum.php
@@ -8,7 +8,7 @@ $request = json_decode($postdata);
 
 $zip = new ZipArchive();
 
-$filename = "allAlbumn.zip";
+$filename = 'zip-'.time().'.zip';
 if($zip->open($filename, ZipArchive::CREATE)!=TRUE)
     die ("Could not open archive");
 
