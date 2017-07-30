a59af9c022c8fd1788f0a2767662c7ee1b1908f9
Bhautik Gosai <bhautikng143@gmail.com>
2017-07-30 20:07:17 +0530

    final

--- upload.php
+++ upload.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once 'google-api-php-client-master/src/Google/autoload.php';
-
+require_once 'google-api-php-client/src/Google_Client.php';
+    require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
     $client = new Google_Client();
     $client->setClientId('207582988644-ukqtahmngraq5963p19mi5u91t3kvf4r.apps.googleusercontent.com');
     $client->setClientSecret('MkhSpAhrUARWSZAokYCx9HzF');
