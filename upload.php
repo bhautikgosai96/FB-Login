a9ef39a6b209d6c70a928b879eea9a6b9f272585
Bhautik Gosai <bhautikng143@gmail.com>
2017-07-30 22:52:17 +0530

    final

--- upload.php
+++ upload.php
@@ -18,3 +18,15 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
+
+$fileMetadata = new Google_Service_Drive_DriveFile(array(
+  'name' => 'photo.jpg'));
+$content = file_get_contents('files/try.jpg');
+$file = $driveService->files->create($fileMetadata, array(
+  'data' => $content,
+  'mimeType' => 'image/jpeg',
+  'uploadType' => 'multipart',
+  'fields' => 'id'));
+printf("File ID: %s\n", $file->id);
+
+?>
