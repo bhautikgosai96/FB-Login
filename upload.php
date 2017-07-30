165649bad1f7762e90dce2e765bd34527115092e
Bhautik Gosai <bhautikng143@gmail.com>
2017-07-30 22:36:12 +0530

    final

--- upload.php
+++ upload.php
@@ -13,6 +13,7 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $drive = new Google_Service_Drive($client);
   $files = $drive->files->listFiles(array())->getItems();
   echo json_encode($files);
+  print_r($file);
 } else {
   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
