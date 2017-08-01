d23cb15124316a4a9cdd0ebe6a32999f5dcd4e55
Bhautik Gosai <bhautikng143@gmail.com>
2017-08-01 17:05:42 +0530

    final

--- sendAllData.php
+++ sendAllData.php
@@ -4,16 +4,10 @@ $data = file_get_contents('php://input');
 $request = json_decode($data);
 session_start();
 
-$userName = $request[0];
-$len = sizeof($request);
-$albumnList = array();
+$userName = $request->userName;
+$albumnList = $request->albumnList;
 
-for($i=1;$i<$len;$i++){
-    array_push($albumnList,$request[$i]);
-}
-
-
-$_SESSION['driveAlbumnList']=$request;
+$_SESSION['driveAlbumnList']=$albumnList;
 $_SESSION['DriveUserName']=$userName;
 
 echo json_encode("Successfully Done!!");
