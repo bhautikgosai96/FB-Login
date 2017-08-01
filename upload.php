aa1618610cb7eb52a60e6148e47d53770b02db1a
Bhautik Gosai <bhautikng143@gmail.com>
2017-08-01 13:37:54 +0530

    final

--- upload.php
+++ upload.php
@@ -2,14 +2,18 @@
 
     require_once 'google-api-php-client/src/Google_Client.php';
     require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
-
+    session_start();
     $driveAlbumnName = $_SESSION['driveAlbumn'];
     $driveUserName = $_SESSION['DriveUserName'];
     $driveImg = $_SESSION['driveImg'];
 
     print_r($driveAlbumnName);
-    print_r(driveUserName);
-    print_r(driveImg);
+    print_r($driveUserName);
+    print_r($driveImg);
+    echo $driveAlbumnName;
+    echo $driveUserName;
+    echo $driveImg;
+
     $client = new Google_Client();
 
     $client->setClientId('207582988644-ukqtahmngraq5963p19mi5u91t3kvf4r.apps.googleusercontent.com');
