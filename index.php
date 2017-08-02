269ebbac661b8d8cf53d33e979a0ae0e1884811b
Bhautik Gosai <bhautikng143@gmail.com>
2017-08-02 21:49:23 +0530

    final

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ session_write_close();
 
 <html>
     <head>
+    <meta name="google-site-verification" content="tr5Orj33t69IRYMBswGtHyhiaQe3CA5B9g65nOXZl4k" />
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
