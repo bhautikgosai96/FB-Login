b842d8d8ebf0912031e453c0a75ebf8f098101c3
Bhautik Gosai <bhautikng143@gmail.com>
2017-08-10 00:04:36 +0530

    final

--- info.php
+++ info.php
@@ -78,33 +78,27 @@ if ($fb->next($photos)) {
 
 
 
-/*foreach($lst as $oneAlbumn){
+/*try {
+                    $response1 = $fb->get('936566536385825/photos?fields=source&limit=500');
+                     $response1 = $fb->get('\''.$albumnId.'/photos?fields=source&limit=500'.'\'');
+                      //$photos = $response->getGraphEdge();
+                    } catch(Facebook\Exceptions\FacebookResponseException $e) {
 
+                    // When Graph returns an error
+                    echo 'Graph returned an error: ' . $e->getMessage();
+                    exit;
+                    } catch(Facebook\Exceptions\FacebookSDKException $e) {
 
-        $albumnId = $oneAlbumn->id;
+                    // When validation fails or other local issues
+                    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                    exit;
+                    }
+              $lst1= $response1->getGraphEdge()->AsArray();
 
+              $albumnObj->albumnName = $oneAlbumn->name;
+              $albumnObj->photo_count = $oneAlbumn->count;
+              $albumnObj->photo_list = $lst1;
 
-        try {
-              $response1 = $fb->get('936566536385825/photos?fields=source&limit=500');
-                //$photos = $response->getGraphEdge();
-              } catch(Facebook\Exceptions\FacebookResponseException $e) {
+              $obj = json_encode($albumnObj);
 
-              // When Graph returns an error
-              echo 'Graph returned an error: ' . $e->getMessage();
-              exit;
-              } catch(Facebook\Exceptions\FacebookSDKException $e) {
-
-              // When validation fails or other local issues
-              echo 'Facebook SDK returned an error: ' . $e->getMessage();
-              exit;
-              }
-        $lst1= $response1->getGraphEdge()->AsArray();
-
-        $albumnObj->albumnName = $oneAlbumn->name;
-        $albumnObj->photo_count = $oneAlbumn->count;
-        $albumnObj->photo_list = $lst1;
-
-        $obj = json_encode($albumnObj);
-
-        $all_array = array_merge($all_array,array($obj))
-    }*/
+              $all_array = array_merge($all_array,array($obj))*/
