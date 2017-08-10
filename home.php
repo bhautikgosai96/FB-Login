956f6917bc4ade23ce9174962e648d78a02b0ae8
Bhautik Gosai <bhautikng143@gmail.com>
2017-08-10 09:03:34 +0530

    final

--- home.php
+++ home.php
@@ -33,32 +33,7 @@ try {
 
 
             $albumnId = $lst[$i]["id"];
-
-            try {
-                                //$response1 = $fb->get('936566536385825/photos?fields=source&limit=500');
-                                 $response1 = $fb->get('\''.$albumnId.'/photos?fields=source&limit=500'.'\'');
-                                  //$photos = $response->getGraphEdge();
-                                } catch(Facebook\Exceptions\FacebookResponseException $e) {
-
-                                // When Graph returns an error
-                                echo 'Graph returned an error: ' . $e->getMessage();
-                                exit;
-                                } catch(Facebook\Exceptions\FacebookSDKException $e) {
-
-                                // When validation fails or other local issues
-                                echo 'Facebook SDK returned an error: ' . $e->getMessage();
-                                exit;
-                                }
-                          $lst1= $response1->getGraphEdge()->AsArray();
-
-                          $albumnObj->albumnName = $lst[$i]["name"];
-                          $albumnObj->photo_count = $lst[$i]["count"];
-                          $albumnObj->photo_list = $lst1;
-
-                          $obj = json_encode($albumnObj);
-
-                          $all_array = array_merge($all_array,array($obj))
-
+            echo '\''.$albumnId.'/photos?fields=source&limit=500'.'\'';
 
 
         }
