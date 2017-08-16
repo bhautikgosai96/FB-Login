eec1722c691661560956af726f9be728d4435d84
Bhautik Gosai <bhautikng143@gmail.com>
2017-08-16 23:32:18 +0530

    final

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 
  $somearg = escapeshellarg('blah');
 
- exec("php https://bhautikng143.herokuapp.com/downloadAllAlbumn.php > /dev/null &", $output);
+ exec("php https://bhautikng143.herokuapp.com/downloadAllAlbumn.php > /dev/null 2>&1 &", $output);
 
  echo json_encode($output);
  ?>
