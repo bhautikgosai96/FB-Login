8642ef5ef45100c9759ef354bf6ca983f0c6f12f
Bhautik Gosai <bhautikng143@gmail.com>
2018-01-15 00:29:16 +0530

    final

--- my.php
+++ my.php
@@ -3,9 +3,9 @@
 session_start();
 var_dump($_SESSION);
 $request= $_SESSION["album"];
-echo $_SESSION["album"];
+print_r($_SESSION["album"]);
 echo "This is my.php";
 echo $request;
-
+print_r($request);
 
 ?>
