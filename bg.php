82c933c773b763b54bb1ec2c8e89aa409bf85e8e
Bhautik Gosai <bhautikng143@gmail.com>
2018-01-18 01:28:08 +0530

    final

--- bg.php
+++ bg.php
@@ -14,7 +14,7 @@ $pass_album = escapeshellarg($r);
 //$status = exec($command);
 
 //$path = "/app/php/bin/php";
-$output = shell_exec("php my.php '$r'");
+$output = shell_exec("php downloadAllAlbum.php $r");
 
 echo json_encode($output);
 
